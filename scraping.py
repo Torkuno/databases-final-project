18393e4958f87120d00c1c75c272e7d30a532d7f
Torkuno <tmesalles@hotmail.com>
2023-12-02 13:25:27 +0100

    Create scraping.py

---
+++ scraping.py
@@ -0,0 +1,41 @@
+import requests
+from bs4 import BeautifulSoup
+import json
+
+url = 'https://www.fourvenues.com/en/discotecas-madrid/events?date=2023-12'
+
+response = requests.get(url)
+
+# Initialize an empty list to store events
+events = []
+
+# Check if the request was successful
+if response.status_code == 200:
+    # Parse the HTML content of the page
+    soup = BeautifulSoup(response.content, 'html.parser')
+
+    # Extract event details
+    for event_div in soup.find_all("div", class_="relative flex transition-all duration-300 hover:shadow-lg shadow-white cursor-pointer bg-gray-100 dark:bg-gray-800 hover:bg-white/70 hover:dark:bg-gray-700/70 rounded-lg overflow-hidden mt-3 cursor-pointer"):
+        # Extracting event name, date, time, and location
+        event_name = event_div.find("p", class_="mt-1 sm:mt-3 font-semibold text-xl sm:text-2xl text-black dark:text-white sm:w-full sm:text-clip").get_text(strip=True)
+        date_div = event_div.find("div", class_="subtitle badge rounded text-xs sm:text-sm bg-secondary text-white p-1 sm:px-2")
+        event_date = " ".join(date_div.stripped_strings)
+        time_div = event_div.find("div", class_="subtitle text-xs sm:text-sm")
+        event_time = time_div.get_text(strip=True)
+        location_div = event_div.find("div", class_="mt-1 badge rounded text-xs sm:text-sm bg-blue-200/30 dark:bg-blue-700/30 text-blue-600 dark:text-blue-100/50 p-1 px-2 whitespace-nowrap")
+        event_location = location_div.get_text(strip=True).replace("\uf3c5", "")  # Removing icon character
+
+        # Adding event details to the list
+        events.append({
+            "name": event_name,
+            "date": event_date,
+            "time": event_time,
+            "location": event_location
+        })
+
+    # Save the events to a JSON file
+    with open('events.json', 'w') as file:
+        json.dump(events, file, indent=4)
+
+else:
+    print("Failed to retrieve the website")
